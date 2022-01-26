f98819f7852cfec6786525053f0bafa792ce70e5
Vincent Biret <vincentbiret@hotmail.com>
2022-01-26 12:30:27 -0500

    - normalizes the string representation by wrapping around smaller units if required (#11)
    * - normalizes the string representation by wrapping around smaller units if required
    
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>
    
    * - fixes typo in unit test
    
    * - makes the normalize method public

--- duration/main.go
+++ duration/main.go
@@ -83,7 +83,7 @@ func FromString(dur string) (*Duration, error) {
 			d.Seconds = int(s)
 			d.MilliSeconds = int(milli * 1000)
 		default:
-			return nil, errors.New(fmt.Sprintf("unknown field %s", name))
+			return nil, fmt.Errorf("unknown field %s", name)
 		}
 	}
 
@@ -91,12 +91,12 @@ func FromString(dur string) (*Duration, error) {
 }
 
 // String prints out the value passed in. It's not strictly according to the
-// ISO spec, but it's pretty close. In particular, to completely conform it
-// would need to round up to the next largest unit. 61 seconds to 1 minute 1
-// second, for example. It would also need to disallow weeks mingling with
+// ISO spec, but it's pretty close. It would need to disallow weeks mingling with
 // other units.
 func (d *Duration) String() string {
 	var s bytes.Buffer
+
+	d.Normalize()
 
 	err := tmpl.Execute(&s, d)
 	if err != nil {
@@ -104,6 +104,41 @@ func (d *Duration) String() string {
 	}
 
 	return s.String()
+}
+
+// Normalize makes sure that all fields are represented as the smallest meaningful value possible by dividing them out by the conversion factor to the larger unit.
+// e.g. if you have a duration of 10 day, 25 hour, and 61 minute, it will be normalized to 1 week 5 days, 2 hours, and 1 minute.
+// this function does not normalize days to months, weeks to months or weeks to years as they do not always convert with the same value.
+func (d *Duration) Normalize() {
+	msToS := 1000
+	StoM := 60
+	MtoH := 60
+	HtoD := 24
+	DtoW := 7
+	if d.MilliSeconds >= msToS {
+		d.Seconds += d.MilliSeconds / msToS
+		d.MilliSeconds %= msToS
+	}
+	if d.Seconds >= StoM {
+		d.Minutes += d.Seconds / StoM
+		d.Seconds %= StoM
+	}
+	if d.Minutes >= MtoH {
+		d.Hours += d.Minutes / MtoH
+		d.Minutes %= MtoH
+	}
+	if d.Hours >= HtoD {
+		d.Days += d.Hours / HtoD
+		d.Hours %= HtoD
+	}
+	if d.Days >= DtoW {
+		d.Weeks += d.Days / DtoW
+		d.Days %= DtoW
+	}
+	//TODO convert 12 months to 1 year when month is supported
+	// a month is not always 30 days, so we don't normalize that
+	// a month is not always 4 weeks, so we don't normalize that
+	// a year is not always 52 weeks, so we don't normalize that
 }
 
 func (d *Duration) HasTimePart() bool {
@@ -116,6 +151,7 @@ func (d *Duration) ToDuration() time.Duration {
 
 	tot := time.Duration(0)
 
+	d.Normalize()
 	tot += year * time.Duration(d.Years)
 	tot += day * 7 * time.Duration(d.Weeks)
 	tot += day * time.Duration(d.Days)
