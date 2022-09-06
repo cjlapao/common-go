c5b1dd7f5377adce8494944538216381cbf4e351
Carlos Lapao <carlos.lapao@ivanti.com>
2022-09-06 14:50:34 +0100

    added linux helper

---
+++ helper/linux_user/main.go
@@ -0,0 +1,132 @@
+package linux_user
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/cjlapao/common-go/commands"
+)
+
+type LinuxUser struct {
+	ID            int
+	Name          string
+	GroupId       int
+	HomeDirectory string
+	Shell         string
+}
+
+type LinuxUserCreateOptions struct {
+	Key   string
+	Value string
+}
+
+func Marshal(value string) (*LinuxUser, error) {
+	if value == "" {
+		return nil, errors.New("value is empty")
+	}
+
+	parts := strings.Split(value, ":")
+	if len(parts) != 7 {
+		return nil, errors.New("wrong format")
+	}
+
+	userId, err := strconv.Atoi(parts[2])
+	if err != nil {
+		return nil, errors.New("the user id is invalid")
+	}
+
+	groupId, err := strconv.Atoi(parts[3])
+	if err != nil {
+		return nil, errors.New("the group id is invalid")
+	}
+
+	result := LinuxUser{
+		ID:            userId,
+		Name:          parts[0],
+		GroupId:       groupId,
+		HomeDirectory: parts[5],
+		Shell:         parts[6],
+	}
+
+	return &result, nil
+}
+
+func Exists(userName string) bool {
+	_, err := Get(userName)
+
+	return err == nil
+}
+
+func Get(userName string) (*LinuxUser, error) {
+	output, err := commands.Execute("getent", "passwd", userName)
+	if err != nil {
+		return nil, errors.New("user does not exist")
+	}
+
+	if output == "" {
+		return nil, errors.New("user does not exist")
+	}
+
+	user, err := Marshal(output)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func UserGroupCreateOption(groupName string) LinuxUserCreateOptions {
+	return LinuxUserCreateOptions{
+		Key:   "-g",
+		Value: groupName,
+	}
+}
+
+func UserHomeDirectoryCreateOption(path string) LinuxUserCreateOptions {
+	return LinuxUserCreateOptions{
+		Key:   "-d",
+		Value: path,
+	}
+}
+
+func Create(userName string, userId int, options ...LinuxUserCreateOptions) error {
+	if userName == "" {
+		return errors.New("group name cannot be empty")
+	}
+
+	if userId <= 1 {
+		return errors.New("group Id needs to be greater than 0")
+	}
+
+	exists := Exists(userName)
+	if exists {
+		return fmt.Errorf("user %v already exists", userName)
+	}
+
+	createParameters := make([]string, 0)
+	createParameters = append(createParameters, "-u")
+	createParameters = append(createParameters, fmt.Sprintf("%v", userId))
+	createParameters = append(createParameters, userName)
+
+	if len(options) > 0 {
+		for _, option := range options {
+			createParameters = append(createParameters, option.Key)
+			createParameters = append(createParameters, option.Value)
+		}
+	}
+
+	output, err := commands.Execute("useradd", createParameters...)
+
+	if err != nil {
+		return fmt.Errorf("there was an error creating user %v with id %v, err %v", userName, userId, err.Error())
+	}
+
+	if strings.ContainsAny(output, "already exists") {
+		return fmt.Errorf("there was an error creating user %v with id %v, user already exists", userName, userId)
+	}
+
+	return nil
+}
---
+++ helper/linux_user_group/main.go
@@ -0,0 +1,90 @@
+package linux_user_group
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/cjlapao/common-go/commands"
+)
+
+type LinuxUserGroup struct {
+	ID   int
+	Name string
+}
+
+func Marshal(value string) (*LinuxUserGroup, error) {
+	if value == "" {
+		return nil, errors.New("value is empty")
+	}
+
+	parts := strings.Split(value, ":")
+	if len(parts) != 4 {
+		return nil, errors.New("wrong format")
+	}
+
+	groupId, err := strconv.Atoi(parts[2])
+	if err != nil {
+		return nil, errors.New("the group id is invalid")
+	}
+
+	result := LinuxUserGroup{
+		ID:   groupId,
+		Name: parts[0],
+	}
+
+	return &result, nil
+}
+
+func Exists(groupName string) bool {
+	_, err := Get(groupName)
+
+	return err == nil
+}
+
+func Get(groupName string) (*LinuxUserGroup, error) {
+	output, err := commands.Execute("getent", "group", groupName)
+	if err != nil {
+		return nil, errors.New("group does not exists")
+	}
+
+	if output == "" {
+		return nil, errors.New("group does not exists")
+	}
+
+	group, err := Marshal(output)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return group, nil
+}
+
+func Create(groupName string, groupId int) error {
+	if groupName == "" {
+		return errors.New("group name cannot be empty")
+	}
+
+	if groupId <= 1 {
+		return errors.New("group Id needs to be greater than 0")
+	}
+
+	exists := Exists(groupName)
+	if exists {
+		return fmt.Errorf("group %v already exists", groupName)
+	}
+
+	output, err := commands.Execute("groupadd", "-g", fmt.Sprintf("%v", groupId), groupName)
+
+	if err != nil {
+		return fmt.Errorf("there was an error creating group %v with id %v, err %v", groupName, groupId, err.Error())
+	}
+
+	if strings.ContainsAny(output, "already exists") {
+		return fmt.Errorf("there was an error creating group %v with id %v, group already exists", groupName, groupId)
+	}
+
+	return nil
+}
