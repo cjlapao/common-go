6bbda7b8a258fb0aef659d7cbcfb924a3e8af831
Carlos Lapao <cjlapao@gmail.com>
2022-11-17 19:24:49 +0000

    removed the restapi namespace, moved to its own repo

--- restapi/entities.go
+++
@@ -1,7 +0,0 @@
-package restapi
-
-type DefaultHomepage struct {
-	CorrelationID string `json:"id" bson:"_id" yaml:"id"`
-	Timestamp     string `json:"timestamp" bson:"timestamp" yaml:"timestamp"`
-	Message       string `json:"message" bson:"message" yaml:"message"`
-}
