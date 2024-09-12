09c58cc994c432bef6a8259ca814e8ee5a51cafb
Carlos Lapao <cjlapao@gmail.com>
2024-09-12 13:02:53 +0100

    Updating Validators for the email address

--- validators/main.go
+++ validators/main.go
@@ -4,7 +4,7 @@ import "regexp"
 
 // ValidateEmailAddress Validates an email string using a regular expression
 func ValidateEmailAddress(email string) bool {
-	exp := regexp.MustCompile(`^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$`)
+	exp := regexp.MustCompile(`^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z]{1,10})|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$`)
 	result := exp.MatchString(email)
 	return result
 }
